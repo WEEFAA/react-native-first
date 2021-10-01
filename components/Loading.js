1c879053bf8e84ca40b806aa2f73516d7771a0dd
Ael <aelfestijo@gmail.com>
2021-10-01 17:58:08 +0800

    [Loading] add background to loading

--- components/Loading.js
+++ components/Loading.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { ActivityIndicator, StyleSheet, View } from 'react-native'
+import { ActivityIndicator, StyleSheet, View, ImageBackground } from 'react-native'
 import Theme, { BlackCoral } from './../styles'
 
 const styles = StyleSheet.create({
@@ -12,7 +12,7 @@ const styles = StyleSheet.create({
 
 const container = StyleSheet.compose(styles.container, Theme.bg_egg)
 export const Loading = function(props){
-    return <View style={container}>
+    return <ImageBackground source={require('./../assets/background.jpeg')} style={container}>
         <ActivityIndicator size="large" color={BlackCoral} {...props}/>
-    </View>
+    </ImageBackground>
 }
