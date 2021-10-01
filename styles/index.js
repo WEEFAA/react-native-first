fa0a8f8c6bfc6169bb328e1fbc95f85b8aebe515
Ael <aelfestijo@gmail.com>
2021-10-01 17:19:11 +0800

    [THEME] add some default theme

---
+++ styles/index.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { StyleSheet } from 'react-native'
+
+export const EggShell = "#F0EBD8"
+export const RichBlack = "#0D1321"
+export const PrussianBlue = "#1D2D44"
+export const BlackCoral = "#3E5C76"
+export const ShadowBlue = "#748CAB"
+
+export default StyleSheet.create({
+    text_egg: {
+        color: ShadowBlue
+    },
+    text_rich_black: {
+        color: RichBlack
+    },
+    text_prussian_blue: {
+        color: PrussianBlue
+    },
+    text_black_coral: {
+        color: BlackCoral
+    },
+    text_shadow_blue: {
+        color: ShadowBlue
+    }
+})
