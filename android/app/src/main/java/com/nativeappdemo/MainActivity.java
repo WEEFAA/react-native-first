d4de825a459c24112fad0257b2aee2f3ad10e95d
Ael <aelfestijo@gmail.com>
2021-10-01 18:32:42 +0800

    [ReactNavigationConfig] modify MainActivity.java for React Navigation

--- android/app/src/main/java/com/nativeappdemo/MainActivity.java
+++ android/app/src/main/java/com/nativeappdemo/MainActivity.java
@@ -1,6 +1,7 @@
 package com.nativeappdemo;
 
 import com.facebook.react.ReactActivity;
+import android.os.Bundle;import android.os.Bundle;
 
 public class MainActivity extends ReactActivity {
 
@@ -11,5 +12,10 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "NativeAppDemo";
+  }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(null);
   }
 }
