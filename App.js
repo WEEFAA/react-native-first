b1295ef1b01f04f7bb001e77331e42d2cb1983f5
Ael <aelfestijo@gmail.com>
2021-10-02 20:33:08 +0800

    [StackNavigator] changed theme of stack navigator

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ import MovieDetails from './screens/MovieDetails';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import NAVS from './navigations'
+import Theme from './styles'
 
 const Stack = createNativeStackNavigator()
 const Tabs = createBottomTabNavigator()
@@ -26,8 +27,13 @@ const PopularMoviesStack = () => {
     title: props.route.params.title
   })
 
+  const options = { 
+    headerStyle: Theme.bg_rich_black, 
+    headerTitleStyle: Theme.text_egg
+  }
+  
   return <View style={styles.container}>
-    <Stack.Navigator initialRouteName={NAVS.POPULAR_MOVIES}>
+    <Stack.Navigator initialRouteName={NAVS.POPULAR_MOVIES} screenOptions={options}>
       <Stack.Screen name={NAVS.POPULAR_MOVIES} component={PopularMovies} />
       <Stack.Screen name={NAVS.MOVIE_DETAIL} component={MovieDetails} options={detailOptions} />
     </Stack.Navigator>
