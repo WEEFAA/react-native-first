ce47849294936271113400a818a522083a6da177
Ael <aelfestijo@gmail.com>
2021-10-02 19:22:27 +0800

    [PopularMovies] add useMemo in state

--- components/Movies.js
+++ components/Movies.js
@@ -1,13 +1,13 @@
-import React from 'react'
-import { movies as MoviesApi, MOVIE_POSTER_HOST } from './../utils/axios'
-import { Push } from './FlatList'
-import { Text, View, StyleSheet, Image, Button, Alert } from 'react-native'
-import Theme, { PrussianBlue } from './../styles'
-import { Loading } from './Loading'
-import { Container } from './Container'
-import NAVS from './../navigations'
+import React from 'react';
+import { movies as MoviesApi, MOVIE_POSTER_HOST } from './../utils/axios';
+import { Push } from './FlatList';
+import { Text, View, StyleSheet, Image, Button, Alert } from 'react-native';
+import Theme, { PrussianBlue } from './../styles';
+import { Loading } from './Loading';
+import { Container } from './Container';
+import NAVS from './../navigations';
 
-const PopularMovieContext = React.createContext({})
+const PopularMovieContext = React.createContext({});
 
 const styles = StyleSheet.create({
     itemsContainer: {
@@ -23,7 +23,7 @@ const styles = StyleSheet.create({
     item: {
         flexDirection: 'row',
         justifyContent: 'flex-start',
-        marginBottom: 8
+        marginBottom: 8,
     },
     // image
     imageWrapper: {
@@ -33,7 +33,7 @@ const styles = StyleSheet.create({
     },
     itemImage: {
         width: '100%',
-        height: '100%'
+        height: '100%',
     },
     // information
     informationWrapper: {
@@ -44,7 +44,7 @@ const styles = StyleSheet.create({
     information: {
         flex: 1,
         flexDirection: 'column',
-        alignItems: 'flex-start'
+        alignItems: 'flex-start',
     },
     title: {
         fontSize: 24,
@@ -54,83 +54,105 @@ const styles = StyleSheet.create({
     description: {
         minHeight: 30,
         fontSize: 16,
-        marginBottom: 5
+        marginBottom: 5,
     },
     learnMoreWrapper: {
-        width: '50%'
-    }
-    
-})
+        width: '50%',
+    },
+});
 
-const title = StyleSheet.compose(styles.title, Theme.text_rich_black)
-const description = StyleSheet.compose(styles.description, Theme.text_rich_black)
+const title = StyleSheet.compose(styles.title, Theme.text_rich_black);
+const description = StyleSheet.compose(
+    styles.description,
+    Theme.text_rich_black,
+);
 
-export const PopularMovies = function(props){
-    const [movies, setMovies] = React.useState([])
-    const [loading, toggleLoading] = React.useState(false)
-    const getMovies = async function(){
-        try{
-            toggleLoading(true)
-            const resp = await MoviesApi.get('/movie/popular')
-            const { results } = resp.data 
-            setMovies(results)
-            toggleLoading(false)
-        }catch(e){
-            toggleLoading(false)
+export const PopularMovies = function (props) {
+    const [movies, setMovies] = React.useState([]);
+    const [loading, toggleLoading] = React.useState(false);
+    const getMovies = async function () {
+        try {
+            toggleLoading(true);
+            const resp = await MoviesApi.get('/movie/popular');
+            const { results } = resp.data;
+            setMovies(results);
+            toggleLoading(false);
+        } catch (e) {
+            toggleLoading(false);
         }
-    }
-    
-    
+    };
 
     React.useEffect(() => {
-        getMovies()
-    },[])
+        getMovies();
+    }, []);
 
-    const state = { movies, loading }
-    return <PopularMovieContext.Provider value={state}>
-        <Container>
-            { props.children }
-        </Container>
-    </PopularMovieContext.Provider>
-}
+    const state = React.useMemo(() => {
+        return {
+            movies,
+            loading,
+        };
+    }, [loading, movies]);
+    return (
+        <PopularMovieContext.Provider value={state}>
+            <Container>{props.children}</Container>
+        </PopularMovieContext.Provider>
+    );
+};
 
-export const Items = function(props){
-    const state = React.useContext(PopularMovieContext)
+export const Items = function (props) {
+    const state = React.useContext(PopularMovieContext);
 
-    const onPressLearnMore = function(id, title){
+    const onPressLearnMore = function (id, title) {
         // redirect to details page
         return e => {
-            props.navigation.navigate(NAVS.MOVIE_DETAIL, { id, title })
-        }
-    }
+            props.navigation.navigate(NAVS.MOVIE_DETAIL, { id, title });
+        };
+    };
 
     const renderItem = ({ item }) => {
-        return <View style={styles.item} key={item.id}>
-            <View style={styles.imageWrapper}>
-                <Image source={{uri: `${MOVIE_POSTER_HOST}${item.poster_path}`}} style={styles.itemImage}/>
-            </View>
-            <View style={styles.informationWrapper}>
-                <View style={styles.information}>
-                    <Text numberOfLines={1} style={title}>{item.title}</Text>
-                    <Text numberOfLines={2} style={description}>{item.overview || "No Description"}</Text>
-                    <Text>Votes: {item.vote_count}</Text>
-                    <Text>Popularity: {Math.ceil(item.popularity)}</Text>
-                </View>
-                <View style={styles.learnMoreWrapper}>
-                    <Button
-                        onPress={onPressLearnMore(item.id, item.title)}
-                        title="Learn More"
-                        color={PrussianBlue}
-                        accessibilityLabel="Learn more about this movie"
+        return (
+            <View style={styles.item} key={item.id}>
+                <View style={styles.imageWrapper}>
+                    <Image
+                        source={{
+                            uri: `${MOVIE_POSTER_HOST}${item.poster_path}`,
+                        }}
+                        style={styles.itemImage}
                     />
                 </View>
+                <View style={styles.informationWrapper}>
+                    <View style={styles.information}>
+                        <Text numberOfLines={1} style={title}>
+                            {item.title}
+                        </Text>
+                        <Text numberOfLines={2} style={description}>
+                            {item.overview || 'No Description'}
+                        </Text>
+                        <Text>Votes: {item.vote_count}</Text>
+                        <Text>Popularity: {Math.ceil(item.popularity)}</Text>
+                    </View>
+                    <View style={styles.learnMoreWrapper}>
+                        <Button
+                            onPress={onPressLearnMore(item.id, item.title)}
+                            title="Learn More"
+                            color={PrussianBlue}
+                            accessibilityLabel="Learn more about this movie"
+                        />
+                    </View>
+                </View>
             </View>
+        );
+    };
+
+    if (state.loading) return <Loading />;
+
+    return (
+        <View style={styles.itemsContainer}>
+            <Push
+                data={state.movies}
+                renderItem={renderItem}
+                style={styles.itemsWrapper}
+            />
         </View>
-    }
-    
-    if(state.loading) return <Loading />
-    
-    return <View style={styles.itemsContainer}>
-        <Push data={state.movies} renderItem={renderItem} style={styles.itemsWrapper}/>
-    </View>
-}
+    );
+};
