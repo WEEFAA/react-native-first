ed4df56e71d1d10a417e3d4a0259049d4ec8f0be
Ael <aelfestijo@gmail.com>
2021-10-02 21:13:56 +0800

    [App] add Popular tab Icon

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { View, StyleSheet } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
+import Ionicons from 'react-native-vector-icons/MaterialIcons';
 import PopularMovies from './screens/PopularMovies';
 import MovieDetails from './screens/MovieDetails';
+import NAVS from './navigations'
+import Theme, { EggShell } from './styles'
+// Navigation
+import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
-import NAVS from './navigations'
-import Theme from './styles'
 
 const Stack = createNativeStackNavigator()
 const Tabs = createBottomTabNavigator()
@@ -31,7 +33,7 @@ const PopularMoviesStack = () => {
     headerStyle: Theme.bg_rich_black, 
     headerTitleStyle: Theme.text_egg
   }
-  
+
   return <View style={styles.container}>
     <Stack.Navigator initialRouteName={NAVS.POPULAR_MOVIES} screenOptions={options}>
       <Stack.Screen name={NAVS.POPULAR_MOVIES} component={PopularMovies} />
@@ -46,10 +48,15 @@ const App = () => {
     tabBarStyle: Theme.bg_rich_black, 
     tabBarLabelStyle: Theme.text_egg 
   }
+  const tabBarIcon = () => <Ionicons name="star" size={30} color={EggShell} />
   return (
     <NavigationContainer>
       <Tabs.Navigator screenOptions={options}>
-        <Tabs.Screen name="Popular" component={PopularMoviesStack}/>
+        <Tabs.Screen 
+          name="Popular" 
+          component={PopularMoviesStack} 
+          options={{tabBarIcon}}
+        />
       </Tabs.Navigator>
     </NavigationContainer>
   );
