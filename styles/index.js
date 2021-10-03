06eb7dce9e17f7d5a505dd6ebcf571c78522da31
Ael <aelfestijo@gmail.com>
2021-10-03 19:01:53 +0800

    [Styles] add shadowing utility

--- styles/index.js
+++ styles/index.js
@@ -1,43 +1,55 @@
-import React from 'react'
-import { StyleSheet } from 'react-native'
+import React from 'react';
+import { StyleSheet } from 'react-native';
 
-export const EggShell = "#F0EBD8"
-export const RichBlack = "#0D1321"
-export const PrussianBlue = "#1D2D44"
-export const BlackCoral = "#3E5C76"
-export const ShadowBlue = "#748CAB"
+export const EggShell = '#F0EBD8';
+export const RichBlack = '#0D1321';
+export const PrussianBlue = '#1D2D44';
+export const BlackCoral = '#3E5C76';
+export const ShadowBlue = '#748CAB';
 
 export default StyleSheet.create({
     // text colors
     text_egg: {
-        color: EggShell
+        color: EggShell,
     },
     text_rich_black: {
-        color: RichBlack
+        color: RichBlack,
     },
     text_prussian_blue: {
-        color: PrussianBlue
+        color: PrussianBlue,
     },
     text_black_coral: {
-        color: BlackCoral
+        color: BlackCoral,
     },
     text_shadow_blue: {
-        color: ShadowBlue
+        color: ShadowBlue,
     },
-    // background 
+    // background
     bg_egg: {
-        backgroundColor: EggShell
+        backgroundColor: EggShell,
     },
     bg_rich_black: {
-        backgroundColor: RichBlack
+        backgroundColor: RichBlack,
     },
     bg_prussian_blue: {
-        backgroundColor: PrussianBlue
+        backgroundColor: PrussianBlue,
     },
     bg_black_coral: {
-        backgroundColor: BlackCoral
+        backgroundColor: BlackCoral,
     },
     bg_shadow_blue: {
-        backgroundColor: ShadowBlue
-    }
-})
+        backgroundColor: ShadowBlue,
+    },
+    // shadows
+    shadow1: color => ({
+        shadowColor: color,
+        shadowOffset: {
+            width: 0,
+            height: 7,
+        },
+        shadowOpacity: 0.41,
+        shadowRadius: 9.11,
+        
+        elevation: 5,
+    }),
+});
