c7126ad1676a2af4a82f67dffe868a96f0476629
Ael <aelfestijo@gmail.com>
2021-10-05 19:58:30 +0800

    [Navigation] display the route name when possible

--- components/Navigation.js
+++ components/Navigation.js
@@ -1,6 +1,12 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { SafeAreaView, TouchableOpacity, StyleSheet, Image, View } from 'react-native'
+import {
+    SafeAreaView,
+    TouchableOpacity,
+    StyleSheet,
+    Image,
+    View,
+} from 'react-native'
 import Ionicons from 'react-native-vector-icons/Ionicons'
 import { iOSColors } from 'react-native-typography'
 import { Title } from './Typography'
@@ -10,31 +16,48 @@ const logo = require('./../assets/esom_adaptive_fore.png')
 
 const propTypes = {
     navigation: PropTypes.object.isRequired,
-    options: PropTypes.object.isRequired
+    options: PropTypes.object.isRequired,
 }
 
 const Navigation = ({ navigation, route, options }) => {
     const canGoBack = navigation.canGoBack()
-    
+    const { title } = options
     const main = React.useMemo(() => {
         return isMain(route.name)
-    },[route])
-    
-    if(!canGoBack || main){
-        return <SafeAreaView style={styles.header}>
-            <Image source={logo} style={styles.logo}/>
-            <View style={styles.brand}>
-                <Title darkTheme font="title1" style={{textAlign: 'center'}}>Esom</Title>
-            </View>
-        </SafeAreaView>
+    }, [route])
+
+    const displayText = title || route.name
+    if (!canGoBack || main) {
+        return (
+            <SafeAreaView style={styles.header}>
+                <Image source={logo} style={styles.logo} />
+                <View style={styles.brand}>
+                    <Title
+                        numberOfLine={1}
+                        darkTheme
+                        font="title1"
+                        style={{ textAlign: 'center' }}>
+                        {displayText}
+                    </Title>
+                </View>
+            </SafeAreaView>
+        )
     }
-    
-    return <SafeAreaView style={styles.header}>
-        <TouchableOpacity style={styles.goBack} onPress={navigation.goBack}>
-            <Ionicons name="chevron-back" size={30} color={iOSColors.white}/>
-            <Title darkTheme font="headline">Back</Title>
-        </TouchableOpacity>
-    </SafeAreaView>
+
+    return (
+        <SafeAreaView style={styles.header}>
+            <TouchableOpacity style={styles.goBack} onPress={navigation.goBack}>
+                <Ionicons
+                    name="chevron-back"
+                    size={30}
+                    color={iOSColors.white}
+                />
+                <Title darkTheme font="headline">
+                    Back
+                </Title>
+            </TouchableOpacity>
+        </SafeAreaView>
+    )
 }
 
 Navigation.propTypes = propTypes
@@ -45,14 +68,14 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         alignItems: 'center',
         paddingHorizontal: 10,
-        ...Theme.bg_rich_black
+        ...Theme.bg_rich_black,
     },
     brand: {
         flex: 1,
-    },  
+    },
     logo: {
         position: 'absolute',
-        left: 0, 
+        left: 0,
         top: 0,
         height: 60,
         width: 60,
@@ -61,8 +84,8 @@ const styles = StyleSheet.create({
     goBack: {
         flexDirection: 'row',
         alignItems: 'center',
-        height: '100%'
-    }
+        height: '100%',
+    },
 })
 
 export default Navigation
