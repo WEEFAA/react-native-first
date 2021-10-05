3c6ded9689cdad6af5beb0462819fa3ad71d58f4
Ael <aelfestijo@gmail.com>
2021-10-05 19:21:22 +0800

    [Navigations] add utility for checking main pages

--- navigations/index.js
+++ navigations/index.js
@@ -1,4 +1,5 @@
-export default {
+// main pages
+const pages = {
     // stacks
     HOME: "Home",
     POPULAR_MOVIES: "Popular Movies",
@@ -7,3 +8,10 @@ export default {
     TAB_HOME: "Tab-Home",
     TAB_POPULAR: "Tab-Popular"
 }
+const MAIN_PAGES = [pages.HOME, pages.POPULAR_MOVIES]
+// utility, check if page is main page
+export const isMain = (page) => {
+    return MAIN_PAGES.includes(page)
+} 
+
+export default pages
