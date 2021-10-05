f03229cd3e7ad5c4cdd2163753b879228425c4ab
Ael <aelfestijo@gmail.com>
2021-10-05 18:00:16 +0800

    [Home] update home screen

--- screens/Home.js
+++ screens/Home.js
@@ -1,14 +1,13 @@
-import 'allsettled-polyfill';
-import React, { useState, useEffect, useCallback, useMemo } from 'react';
-import { Container } from './../components/Container';
-import { movies, MOVIE_POSTER_HOST } from './../utils/axios';
-import { Slider } from './../components/Slider';
-import { ListSection } from './../components/Sections';
-import { Text, ScrollView, StyleSheet, View, Dimensions } from 'react-native';
-import { Card } from './../components/Card';
-import { Loading } from './../components/Loading';
-import { RichBlack } from './../styles';
-const dimensions = Dimensions.get('screen');
+import 'allsettled-polyfill'
+import React, { useState, useEffect, useCallback, useMemo } from 'react'
+import { Container } from './../components/Container'
+import { movies, MOVIE_POSTER_HOST } from './../utils/axios'
+import { Slider } from './../components/Slider'
+import { ListSection } from './../components/Sections'
+import { Text, ScrollView, StyleSheet, View, useWindowDimensions } from 'react-native'
+import { Card } from './../components/Card'
+import { Loading } from './../components/Loading'
+import { RichBlack } from './../styles'
 
 const styles = StyleSheet.create({
     others: {
@@ -16,90 +15,90 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         paddingHorizontal: 7,
     },
-});
+})
 
 const Home = function (props) {
     // loading
-    const [sliderLoading, toggleSliderLoading] = useState(true);
-    const [othersLoading, toggleOthersLoading] = useState(true);
+    const dimensions = useWindowDimensions()
+    const [sliderLoading, toggleSliderLoading] = useState(true)
+    const [othersLoading, toggleOthersLoading] = useState(true)
     // data
-    const [topRated, setTopRated] = useState([]);
-    const [popular, setPopular] = useState([]);
-    const [nowPlaying, setNowPlaying] = useState([]);
-    const [upcoming, setUpcoming] = useState([]);
+    const [topRated, setTopRated] = useState([])
+    const [popular, setPopular] = useState([])
+    const [nowPlaying, setNowPlaying] = useState([])
+    const [upcoming, setUpcoming] = useState([])
 
     const fetchTopRated = useCallback(async () => {
         try {
-            toggleSliderLoading(true);
+            toggleSliderLoading(true)
             const resp = await movies.get('/movie/top_rated', {
                 params: { page: 1 },
-            });
-            const { results } = resp.data;
-            toggleSliderLoading(false);
-            setTopRated(results);
+            })
+            const { results } = resp.data
+            toggleSliderLoading(false)
+            setTopRated(results)
         } catch (e) {
-            toggleSliderLoading(false);
+            toggleSliderLoading(false)
         }
-    }, []);
+    }, [])
 
     const fetchOthers = useCallback(async () => {
         try {
-            toggleOthersLoading(true);
-            const promises = [];
+            toggleOthersLoading(true)
+            const promises = []
             // fetch other data
             const urls = {
                 upcoming: '/movie/upcoming',
                 now_playing: '/movie/now_playing',
                 popular: '/movie/popular',
-            };
+            }
             const setter = {
                 [urls.upcoming]: setUpcoming,
                 [urls.now_playing]: setNowPlaying,
                 [urls.popular]: setPopular,
-            };
-            promises.push(movies.get(urls.upcoming, { params: { page: 1 } }));
-            promises.push(
-                movies.get(urls.now_playing, { params: { page: 1 } }),
-            );
-            promises.push(movies.get(urls.popular, { params: { page: 1 } }));
+            }
+            promises.push(movies.get(urls.upcoming, { params: { page: 1 } }))
+            promises.push(movies.get(urls.now_playing, { params: { page: 1 } }))
+            promises.push(movies.get(urls.popular, { params: { page: 1 } }))
             // handle
-            const responses = await Promise.allSettled(promises);
+            const responses = await Promise.allSettled(promises)
             responses.forEach((response, index) => {
                 if (response.status === 'fulfilled') {
-                    const resp = response.value;
-                    const url = resp.config.url;
-                    const { results } = resp.data;
+                    const resp = response.value
+                    const url = resp.config.url
+                    const { results } = resp.data
                     if (setter[url]) {
-                        setter[url](results);
+                        setter[url](results)
                     }
                 }
-            });
-            toggleOthersLoading(false);
+            })
+            toggleOthersLoading(false)
         } catch (e) {
-            console.error(e);
-            toggleOthersLoading(false);
+            console.error(e)
+            toggleOthersLoading(false)
         }
-    }, []);
+    }, [])
 
     useEffect(() => {
-        fetchTopRated();
-        fetchOthers();
-    }, []);
+        fetchTopRated()
+        fetchOthers()
+    }, [])
 
     const renderCards = useMemo(() => {
         return ({ item }) => {
-            const uri = `${MOVIE_POSTER_HOST}${item.poster_path}`;
-            const style = { marginRight: 10 };
+            const uri = `${MOVIE_POSTER_HOST}${item.poster_path}`
+            const style = { marginRight: 10 }
             return (
                 <Card item={item} source={{ uri }} wrapperStyle={style}>
                     <Text>Hello</Text>
                 </Card>
-            );
-        };
-    }, []);
+            )
+        }
+    }, [])
 
-    const wrapperStyle = { marginBottom: 20 };
-    const loadingComponentHeight = { height: dimensions.height / 2 };
+    const wrapperStyle = { marginBottom: 20 }
+    const loadingComponentHeight = { height: dimensions.height / 1.5 }
+    const sliderMinHeight = useMemo(() => dimensions.height / 1.5, [dimensions])
     return (
         <Container bgColor={RichBlack}>
             <ScrollView>
@@ -107,6 +106,7 @@ const Home = function (props) {
                     <Loading wrapperStyle={loadingComponentHeight} />
                 ) : (
                     <Slider
+                        minHeight={sliderMinHeight}
                         images={topRated.map(
                             item => `${MOVIE_POSTER_HOST}${item.poster_path}`,
                         )}
@@ -138,7 +138,7 @@ const Home = function (props) {
                 )}
             </ScrollView>
         </Container>
-    );
-};
+    )
+}
 
-export default Home;
+export default Home
