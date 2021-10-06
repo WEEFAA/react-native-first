b1fe8dcc4492eca1bc06316de4f82e97a90dae56
Ael <aelfestijo@gmail.com>
2021-10-06 16:40:18 +0800

    [Navigations] add discover stuff

--- navigations/index.js
+++ navigations/index.js
@@ -4,11 +4,13 @@ const pages = {
     HOME: "Home",
     POPULAR_MOVIES: "Popular Movies",
     MOVIE_DETAIL: "Movie Detail",
+    DISCOVER: "Discover",
     // tabs
     TAB_HOME: "Tab-Home",
-    TAB_POPULAR: "Tab-Popular"
+    TAB_POPULAR: "Tab-Popular",
+    TAB_DISCOVER: "Tab-Discover"
 }
-const MAIN_PAGES = [pages.HOME, pages.POPULAR_MOVIES]
+const MAIN_PAGES = [pages.HOME, pages.POPULAR_MOVIES, pages.DISCOVER]
 // utility, check if page is main page
 export const isMain = (page) => {
     return MAIN_PAGES.includes(page)
