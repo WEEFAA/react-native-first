f0145fb2f9ab9804f3de55b1d3d13c0f201e0557
Ael <aelfestijo@gmail.com>
2021-10-06 18:30:41 +0800

    [Section] only render title when present

--- components/Sections.js
+++ components/Sections.js
@@ -10,49 +10,53 @@ const styles = StyleSheet.create({
         width: '100%',
         alignItems: 'flex-start',
         justifyContent: 'center',
-        paddingVertical: 5,   
+        paddingVertical: 5,
         paddingHorizontal: 5,
         // corners
         borderRadius: 5,
         // shadow
         backgroundColor: iOSColors.black,
-    }, 
+    },
     // header
     sectionHeaderContainer: {
         width: '100%',
         minHeight: 35,
         marginBottom: 3,
-        justifyContent: 'center'
-    }
+        justifyContent: 'center',
+    },
 })
 
 const header = StyleSheet.flatten([material.titleWhite])
-const container = StyleSheet.compose(styles.container,Theme.shadow1(BlackCoral))
+const container = StyleSheet.compose(
+    styles.container,
+    Theme.shadow1(BlackCoral),
+)
 
 const listSectionTypes = {
     wrapperStyle: PropTypes.object,
     title: PropTypes.string,
-    renderItem: PropTypes.func.isRequired
+    renderItem: PropTypes.func.isRequired,
 }
 
-export const ListSection = function(props){
-
+export const ListSection = function (props) {
     const renderItem = useMemo(() => {
-        return ({item}) => {
+        return ({ item }) => {
             return <Text>{item}</Text>
         }
-    },[])
-    
-    return <View style={StyleSheet.compose(container, props.wrapperStyle)}>
-        <View style={styles.sectionHeaderContainer}>
-            <Text numberOfLines={1} style={header}>{props.title}</Text>
+    }, [])
+
+    return (
+        <View style={StyleSheet.compose(container, props.wrapperStyle)}>
+            {props.title && (
+                <View style={styles.sectionHeaderContainer}>
+                    <Text numberOfLines={1} style={header}>
+                        {props.title}
+                    </Text>
+                </View>
+            )}
+            <FlatList horizontal renderItem={renderItem} {...props} />
         </View>
-        <FlatList 
-            horizontal 
-            renderItem={renderItem}
-            {...props}
-        />
-    </View>
+    )
 }
 
 ListSection.propTypes = listSectionTypes
