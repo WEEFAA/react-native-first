fd14d83957647af28095364ba08881847af72f73
Ael <aelfestijo@gmail.com>
2021-10-07 10:29:18 +0800

    [FormInput] add custom prop for styling text

--- components/Form.js
+++ components/Form.js
@@ -1,9 +1,32 @@
 import React from 'react'
 import { TextInput, StyleSheet, View } from 'react-native'
+import PropTypes from 'prop-types'
+import { human, iOSColors } from 'react-native-typography'
 
-export const Input = function({ style, wrapperStyle, children, ...props }){
+const titleFonts = [
+    'largeTitle', 'title1', 'title2', 'title3', 'headline'
+]
+
+const bodyFonts = [
+    'body', 'callout', 'subhead', 'footnote', 'caption1', 'caption2'
+]
+
+const inputPropTypes = {
+    size: PropTypes.oneOf([...titleFonts, ...bodyFonts]),
+    color: PropTypes.string.isRequired,
+    style: PropTypes.object,
+    wrapperStyle: PropTypes.object,
+}
+
+const inputDefaultProps = {
+    size: 'headline',
+    color: iOSColors.black
+}
+
+export const Input = function({ color, size, style, wrapperStyle, children, ...props }){
+    const humanFont = human[size] 
     return <View style={styles.container(wrapperStyle)}>
-        <TextInput style={styles.input(style)} {...props}/>
+        <TextInput style={styles.input(humanFont, color, style)} {...props}/>
         <View style={styles.extra}>
             { children }
         </View>
@@ -20,17 +43,20 @@ const styles = StyleSheet.create({
         },
         style
     ]),
-    input: (style) => StyleSheet.flatten([
+    input: (font, color, style) => StyleSheet.flatten([
         {
-            height: 40,
+            height: 'auto',
             borderWidth: 2,
             padding: 10,
             paddingRight: 20,
             borderRadius: 8,
             flexGrow: 1,
             flexShrink: 1,
-            marginRight: 5
+            marginRight: 5,
+            paddingVertical: 8,
+            color: color
         },
+        font,
         style
     ]),
     extra: {
@@ -38,3 +64,6 @@ const styles = StyleSheet.create({
         alignItems: 'center'
     }
 })
+
+Input.propTypes = inputPropTypes
+Input.defaultProps = inputDefaultProps
